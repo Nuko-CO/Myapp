25c4bd5e71be2385767b579e111145253ac9e3a2
Nuko-CO <nukopoigraem@gmail.com>
2022-12-01 01:31:49 +0600

    6.5 Create a new Dice object instance

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
     }
 
     class Dice(private val numSides: Int) {
